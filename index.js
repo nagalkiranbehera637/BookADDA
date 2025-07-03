4e4c0cb5db019989c481af1625018a30f814beb0
nagalkiranbehera637 <nagalkiranbehera123@gmail.com>
2025-07-03 12:35:23 +0530

    make  edit page complete and date correction in search page

--- index.js
+++ index.js
@@ -57,8 +57,16 @@ app.get("/book/:id", async (req, res) => {
 });
 
 app.get("/edit/:id",async(req,res)=>{
+  try{
       const id=req.params.id;
-      res.render("new.ejs") //cuurently it takes to the new page but we gonna update this soon
+       const result = await db.query("SELECT * FROM books WHERE id=$1", [id]);
+       if (result.rows.length === 0) return res.status(404).send("Book not found");
+      res.render("edit.ejs",{book:result.rows[0]})
+       //currently it takes to the new page but we gonna update this soon
+    } catch (err) {
+    console.error("Error fetching book by ID:", err);
+    res.status(500).send("Server Error");
+  }
 })
 
 app.get("/delete/:id",async(req,res)=>{
@@ -134,6 +142,52 @@ app.get("/sortBytypo", async (req, res) => {
   }
 });
 
+
+app.post("/books/:id",async(req,res)=>{
+  try {
+    const id= req.params.id;
+    const existing_note=await db.query("SELECT * FROM books WHERE id=$1",[id])
+
+    const title = req.body.title;
+    const cover = await axios.get(`https://openlibrary.org/search.json?title=${encodeURIComponent(title)}`);
+    const cover_id = cover.data.docs[0]?.cover_i ||existing_note.rows[0].cover_id;
+
+    const query = `
+  UPDATE books
+  SET
+    title = $1,
+    author = $2,
+    why_should = $3,
+    note = $4,
+    read_date = $5,
+    rating = $6,
+    cover_id = $7,
+    user_id = $8,
+    vote = $9
+  WHERE id = $10
+  RETURNING *;
+`;
+const values = [
+  req.body.title ?? existing_note.rows[0].title,
+  req.body.author ?? existing_note.rows[0].author,
+  req.body.why_should ?? existing_note.rows[0].why_should,
+  req.body.note ?? existing_note.rows[0].note,
+  req.body.read_date ?? existing_note.rows[0].read_date,
+  req.body.rating ?? existing_note.rows[0].rating,
+  cover_id,
+  req.body.user_id ?? existing_note.rows[0].user_id,
+  req.body.vote ?? existing_note.rows[0].vote,
+  id
+    ];
+
+    const result = await db.query(query, values);
+    const date = await getDateFormat(result.rows[0].read_date);
+    res.render("book.ejs", { book: result.rows[0], date });
+  } catch (err) {
+    console.error("Error updating book notes:", err);
+    res.status(500).send("Server Error");
+  }
+})
 app.post("/books", async (req, res) => {
   try {
     const title = req.body.title;
